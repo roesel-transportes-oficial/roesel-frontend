1ed4152cfc1603f59d73081d36a9bde781ea6838
Roeseltransportes <amanda.cardoso@roeseltransportes.com.br>
2026-04-29 09:47:20 -0300

    novo

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  generateEtags: false,
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
-          { key: 'Cache-Control', value: 'no-store, must-revalidate' },
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, max-age=0' },
           { key: 'Pragma', value: 'no-cache' },
-          { key: 'Expires', value: '0' },
+          { key: 'Expires', value: '-1' },
+          { key: 'Surrogate-Control', value: 'no-store' },
         ],
       },
     ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
