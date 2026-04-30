6c8ccd96756187f871495e02ba876c8566f2f775
Roeseltransportes <amanda.cardoso@roeseltransportes.com.br>
2026-04-30 09:59:40 -0300

    Update supabase.ts

--- app/services/supabase.ts
+++ app/services/supabase.ts
@@ -5,8 +5,12 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
     persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
+    detectSessionInUrl: false,
+    flowType: 'implicit',
+    lock: async (name, acquireTimeout, fn) => {
+      return fn()
+    },
+  }
 })
