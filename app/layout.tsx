c4338b102150188184b88ea6f582cad26d3726b6
Roeseltransportes <amanda.cardoso@roeseltransportes.com.br>
2026-08-27 16:12:56 -0300

    n

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,42 +10,11 @@ export const metadata: Metadata = {
   description: 'Sistema interno de gestão',
 }
 
-const APP_VERSION = '1.0.4'
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <head>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            (function() {
-              var VERSION = '${APP_VERSION}';
-              var storedVersion = localStorage.getItem('app_version');
-              
-              if (storedVersion !== VERSION) {
-                localStorage.clear();
-                localStorage.setItem('app_version', VERSION);
-              }
-
-              if ('serviceWorker' in navigator) {
-                navigator.serviceWorker.getRegistrations().then(function(registrations) {
-                  for (var r of registrations) { r.unregister(); }
-                });
-              }
-
-              if ('caches' in window) {
-                caches.keys().then(function(names) {
-                  for (var name of names) { caches.delete(name); }
-                });
-              }
-            })();
-          `
-        }} />
-      </head>
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
